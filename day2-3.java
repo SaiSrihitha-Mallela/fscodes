989e45cd4c2138c5993bc5e81e92c4cd210da845
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-02-10 20:01:52 +0530

    Create day2-3.java

---
+++ day2-3.java
@@ -0,0 +1,16 @@
+Somu is playing a game. The Game Rules are as follows 
+
+A word will be given, where Ramu has to replace the letters of the word with V/C
+If the letter is a vowel it will be de denoted by upper case letter ‘V’ and 
+if the letter is a consonant it will be denoted by upper case letter ‘C’. 
+
+Note - The output will contain a word containing alternating sequence of ‘C’ and ‘V’.
+It is not permissible to have two or more consecutive V or C in Sequence.
+Letters will be only(a-z/A-Z)
+
+input = hello 
+output = CVCV
+
+Explanation - When we convert the above  "hello" we have CVCCV 
+but for the output It is not permissible to have two or more consecutive V or C in Sequence.
+so CVCCV changes to CVCV 
