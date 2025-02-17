212cbe38dc2f40b243d3c137e77a1a9e9249bf6b
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-02-17 14:11:57 +0530

    Create day7-4.java

---
+++ day7-4.java
@@ -0,0 +1,24 @@
+Rahul is given a row of numbers where each row and columns are same. 
+Rahul now prints the numbers of the rows as shown in the following manner. 
+Help Rahul to write the code .
+
+The first line of input consists of the size followed by the row numbers. 
+
+Sample Input:
+3
+1 2 3 
+4 5 6
+7 8 9
+
+
+Explanation:
+
+1<-2<-3 
+|
+4->5->6
+             |
+7<-8<-9
+   
+
+Sample Output: 
+3 2 1 4 5 6 9 8 7
