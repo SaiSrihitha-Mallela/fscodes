14f98dccc309a7fed365ebc95fe25af7b90b89ba
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-02-17 15:17:37 +0530

    Create day8-2.java

---
+++ day8-2.java
@@ -0,0 +1,44 @@
+Amazon fire tv stick keyboard wanted to be operated on number pad. 
+You will be given the below pad. 
+You are supposed to print all possible words in alphabetic order for a given number (0-9)
+
+	1		2		3
+		   abc	  def
+		 
+	4		5		6
+   ghi     jkl    mno
+  
+	7		8		9
+  pqrs     tuv   wxyz
+	
+	*		0		#
+
+
+You will be given a string S which contains (2-9)  only  
+For example: S = "3", then the possible words are "d", "e", "f".
+
+Input Format:
+-------------
+String(2-9)
+
+Output Format:
+--------------
+Print the list of words in alphabetical order
+
+
+Sample Input-1:
+---------------
+2
+
+Sample Output-1:
+----------------
+a b c
+
+
+Sample Input-2:
+---------------
+24
+
+Sample Output-2:
+----------------
+ag ah ai bg bh bi cg ch ci
