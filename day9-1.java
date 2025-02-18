b5c07b4b0e5c91214d4b513294210dd1aae6d14d
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-02-18 20:32:47 +0530

    Update day9-1.java
    two-pointers

--- day9-1.java
+++ day9-1.java
@@ -13,3 +13,35 @@ Input = 1 2 3
 3 2 4 
 output false
 
+import java.util.*;
+
+class day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr1 = sc.nextLine().split(" ");
+        String[] arr2 = sc.nextLine().split(" ");
+
+        List<Integer> list = new ArrayList<>();
+        for (String s : arr1) {
+            list.add(Integer.parseInt(s));
+        }
+        for (String s : arr2) {
+            list.add(Integer.parseInt(s));
+        }
+
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            if (!list.get(left).equals(list.get(right))) {
+                System.out.println(list.get(left)); 
+                System.out.println(list.get(right));
+                System.out.print("false");
+                return;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.print("true");
+    }
+}
