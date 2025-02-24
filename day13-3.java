ecaf5a3e57b67e78041005515adb14da6d9ca308
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-02-24 20:59:17 +0530

    Create day13-3.java

---
+++ day13-3.java
@@ -0,0 +1,20 @@
+Tubby a Third Class Student has given a Assignment on Time Chapter by his class teacher.
+Tubby is supposed to change the time given in AM/PM Format to 24 Hours Format. 
+Help Tubby Solve the Problem 
+  
+The Maths teacher gave him the time in the following Format - HH:MM:SS:AM/PM
+   
+Assume all HH,MM,SS are in the valid range only 
+  
+Note: Midnight is 12:00:00AM on a 12-hour clock,  and 00:00:00 on a 24-hour clock. 
+Noon is 12:00:00PM on a 12-hour clock,  and 12:00:00 on a 24-hour clock. 
+       
+Assume all input times are valid
+    
+
+Example:
+input = 06:10:20PM
+output = 18:10:20
+  
+
+    
