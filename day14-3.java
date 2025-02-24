d88bc5f39acb05a3e9235b9bd39a0c3940564563
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-02-24 22:05:11 +0530

    Update day14-3.java

--- day14-3.java
+++ day14-3.java
@@ -15,6 +15,80 @@ Assume all input times are valid
 Example:
 input = 06:10:20PM
 output = 18:10:20
-  
 
+
+  
+import java.util.*;
+
+class TimeConversion
+{
+    public static String conversion(String time)
+    {
+        StringBuilder hours = new StringBuilder();
+        StringBuilder minutes = new StringBuilder();
+        StringBuilder seconds = new StringBuilder();
+        StringBuilder period = new StringBuilder();
+        int colonCount = 0;
+
+        for(int i=0;i<time.length();i++)
+        {
+            char ch = time.charAt(i);
+
+            if(ch == ':')
+            {
+                colonCount++;
+                continue;
+            }
+
+            if(colonCount == 0)
+            {
+                hours.append(ch);
+            }
+            else if(colonCount == 1)
+            {
+                minutes.append(ch);
+            }
+            else if(colonCount == 2)
+            {
+                if(Character.isDigit(ch))
+                {
+                    seconds.append(ch);
+                }
+                else
+                {
+                    period.append(ch);
+                }
+            }
+        }
+
+        int hrs = Integer.parseInt(hours.toString());
+        String mins = minutes.toString();
+        String secs = seconds.toString();
+        String prd = period.toString();
+
+        if(prd.equals("AM"))
+        {
+            if(hrs == 12)
+            {
+                hrs = 0;
+            }
+        }
+        else{
+            if(hrs != 12)
+            {
+                hrs += 12;
+            }
+        }
+
+        String formate = (hrs < 10) ? "0" + hrs :  String.valueOf(hrs);
+        
+        return formate + ":" + mins + ":" + secs;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.next();
+        System.out.print(conversion(time));
+    }
+}
     
