fb760d9890f7682f6e047780c1f91745cb3fb33a
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-03-05 22:42:15 +0530

    Create day20-1.java

---
+++ day20-1.java
@@ -0,0 +1,25 @@
+Mystic Energy Number
+
+In an ancient civilization, numbers were believed to carry mystical energy. 
+A number is considered blessed if repeatedly replacing it with the sum of the squares of its digits eventually results in 1. 
+However, if it falls into a never-ending cycle that does not include 1, it is considered cursed.
+
+Input
+An integer num (1 ≤ num ≤ 10⁶).
+Output
+Print "Blessed Number" if the number eventually reaches 1.
+Otherwise, print "Cursed Number".
+
+
+Example
+Input = 23  
+Output = Blessed Number
+
+(Explanation: 23 → 2² + 3² = 13 → 1² + 3² = 10 → 1² + 0² = 1, so it is a Blessed Number.)
+
+Input = 25  
+Output = Cursed Number
+(Explanation: 25 falls into a cycle and does not reach 1.)
+
+
+
