6cc29002a64ea158457ddb77e227fb33fe5eb5a0
Sai Srihitha Mallela <139038259+SaiSrihitha-Mallela@users.noreply.github.com>
2025-03-05 22:42:55 +0530

    Create day20-2.java

---
+++ day20-2.java
@@ -0,0 +1,52 @@
+Shankuntala  a maths student a given a set of N numbers, and was asked to to perform the operations listed below:
+
+1. sumRange(start, end) - return the sum of numbers between the indices start and end, both are inclusive.
+2. update(ind, val) - update the value at the index 'ind' to 'val'.
+
+Your task is to solve this problem using Fenwick Tree concept.
+
+Input Format:
+-------------
+Line-1: Two integers N and Q, size of the array(set of numbers) and query count.
+Line-2: N space separated integers.
+next Q lines: Three integers option, start/ind and end/val.
+ol
+Output Format:
+--------------
+An integer result, for every sumRange query.
+
+
+Sample Input-1:
+---------------
+8 5
+1 2 13 4 25 16 17 8
+1 2 6		//sumRange
+1 0 7		//sumRange
+2 2 18	//update
+2 4 17	//update
+1 2 7		//sumRange
+
+Sample Output-1:
+----------------
+75
+86
+80
+
+
+
+Sample Input-2:
+---------------
+8 5
+1 2 13 4 25 16 17 8
+1 2 6		
+1 0 7		
+2 2 18	
+2 4 17	
+1 0 7
+
+Sample Output-2:
+----------------
+75
+86
+83
+
